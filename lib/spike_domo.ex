696708ca964fc17552e91e2b48b80c3b3a19eca6
Radek Szymczyszyn <radek.szymczyszyn@v7labs.com>
2025-05-29 11:39:11 +0200

    Save project structure

---
+++ lib/spike_domo.ex
@@ -0,0 +1,18 @@
+defmodule SpikeDomo do
+  @moduledoc """
+  Documentation for `SpikeDomo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpikeDomo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spike_domo_test.exs
@@ -0,0 +1,8 @@
+defmodule SpikeDomoTest do
+  use ExUnit.Case
+  doctest SpikeDomo
+
+  test "greets the world" do
+    assert SpikeDomo.hello() == :world
+  end
+end
